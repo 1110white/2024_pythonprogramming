76dacf2d8c4309d3dc34d65dbcb42aa68c365469
1110white <162237028+1110white@users.noreply.github.com>
2024-04-19 12:27:36 +0900

    Update pt 8.25 (구구단).py

--- pt 8.25 (구구단).py
+++ pt 8.25 (구구단).py
@@ -9,5 +9,12 @@ def display_multiplication_table(n):
         print(f'{n} x {i} = {n * i:2d}')
         i+= 1
 
+# while 문 사용 :
+# def display_multiplication_table(n):
+    # i = 1
+    # while i <= 9 :
+        # print(f'{n} x {i} = {n * i:2d}')
+        # i += 1
+
 n = input_positive_number('출력할 구구단을 양의 정수로 입력하세요: ')
 display_multiplication_table(n)
