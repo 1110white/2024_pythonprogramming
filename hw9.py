e35857ccab00d6d79e6ee195e2a2d7bbeff1ed5c
1110white <162237028+1110white@users.noreply.github.com>
2024-06-14 14:00:20 +0900

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -1,31 +1,28 @@
+from point import Point
+
 class Rectangle:
     
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+    def __init__(self, lt_x1, lt_y1, rb_x2, rb_y2):
+        self.__lt = Point(lt_x, lt_y)
+        self.__rb = Point(rb_x, rb_y)
         
     def show(self):
-        print(f'좌측 상단 꼭지점이 ({x1},{y1})이고 우측 하단 꼭지점이 ({x2},{y2})인 사각형입니다.')
+        print(f'좌측 상단 꼭지점이 {self.__lt.toString()}이고', end='')
+        print(f'우측 하단 꼭지점이 {self.__rb.toString}인 사각형입니다.')
 
     def getWidth(self):
-        result = self.x2 - self.x1
-        return result
+        return self._rb.getX() - self.__ltgetX()
 
     def getHeight(self):
-        result = self.y2 - self.y1
-        return result
+        ltx, lty = self.__lt.get()
+        rbx, rby = self.__rb.get()
+        return rby - lty
 
     def getArea(self):
-        result = getWidth() * getHeight()
-        return result
-
-    def getPerimeter(self):
-        result = 2*(getWidth() + getHeight())
-        return result
+        return self.getWidth() * self.getHeight()
         
-
+    def getPerimeter(self):
+        return (self.getWidth() + self.getHeight()) * 2
 
 r1 = Rectangle(5, 5, 20, 10)
 a = r1.getArea()
